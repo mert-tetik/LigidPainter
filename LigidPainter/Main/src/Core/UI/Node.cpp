9f3458ef0b9a2e8b50971984ce6f8bcc1100e1c1
mert-tetik <merttetikx@icloud.com>
2023-01-13 20:26:06 +0300

    Implementation of zoom to node positions

--- LigidPainter/Main/src/Core/UI/Node.cpp
+++ LigidPainter/Main/src/Core/UI/Node.cpp
@@ -18,7 +18,7 @@
 bool firstRelease = false;
 
 void UserInterface::node(Node &node,Programs programs,Icons icons,GLFWwindow* window,double mouseX,double mouseY,double xOffset,double yOffset,
-float maxScreenWidth,float maxScreenHeight, std::vector<Node> &nodes){
+float maxScreenWidth,float maxScreenHeight, std::vector<Node> &nodes,float zoomVal){
 	ColorData colorData;
 	Utilities util;
 
@@ -64,7 +64,7 @@ float maxScreenWidth,float maxScreenHeight, std::vector<Node> &nodes){
 		
 
     //Move the node if it's top bar is pressed
-	node.barHover = isMouseOnButton(window , node.width , iconWidth*2.f ,node.positionX,node.positionY + node.height + iconWidth*2.f,mouseX,mouseY,false);
+	node.barHover = isMouseOnButton(window , node.width , iconWidth*2.f ,node.positionX * zoomVal,node.positionY * zoomVal + node.height + iconWidth*2.f,mouseX,mouseY,false);
 	if(glfwGetMouseButton(window,0) == GLFW_PRESS && node.barHover && !anyBarPressed){
 		node.barPressed = true;
 	}
@@ -72,8 +72,8 @@ float maxScreenWidth,float maxScreenHeight, std::vector<Node> &nodes){
 		node.barPressed = false;
 	}
 	if(node.barPressed){
-		node.positionX += xOffset/maxScreenWidth*2.f;
-		node.positionY -= yOffset/maxScreenHeight*2.f;
+		node.positionX += xOffset/maxScreenWidth*2.f/zoomVal;
+		node.positionY -= yOffset/maxScreenHeight*2.f/zoomVal;
 		
 		xOffset = 0;
 		yOffset = 0;
@@ -81,18 +81,18 @@ float maxScreenWidth,float maxScreenHeight, std::vector<Node> &nodes){
 
     //Render the panel
 	glUseProgram(programs.uiProgram);
-	box(node.width,node.height,node.positionX,node.positionY,"",node.backColor,0,0,0,0.9999f,10000,node.backColor,0);
+	box(node.width,node.height,node.positionX * zoomVal,node.positionY * zoomVal,"",node.backColor,0,0,0,0.9999f,10000,node.backColor,0);
     //-Side area
-	box(iconWidth,node.height,node.positionX-node.width -iconWidth,node.positionY,"",node.backColor,0,0,0,0.9999f,10000,node.backColor,0);///Left
-	box(iconWidth,node.height,node.positionX+node.width +iconWidth,node.positionY,"",node.backColor,0,0,0,0.9999f,10000,node.backColor,0);///Right
-	box(node.width,iconWidth*2.f,node.positionX,node.positionY + node.height + iconWidth*2.f,"",node.upBarColor,0,0,0,0.9999f,10000,node.upBarColor,0);///Top
-	box(node.width,iconWidth*2.f,node.positionX,node.positionY - node.height - iconWidth*2.f,"",node.backColor,0,0,0,0.9999f,10000,node.backColor,0);///Bottom
+	box(iconWidth,node.height,node.positionX * zoomVal-node.width -iconWidth,node.positionY * zoomVal,"",node.backColor,0,0,0,0.9999f,10000,node.backColor,0);///Left
+	box(iconWidth,node.height,node.positionX * zoomVal+node.width +iconWidth,node.positionY * zoomVal,"",node.backColor,0,0,0,0.9999f,10000,node.backColor,0);///Right
+	box(node.width,iconWidth*2.f,node.positionX * zoomVal,node.positionY * zoomVal + node.height + iconWidth*2.f,"",node.upBarColor,0,0,0,0.9999f,10000,node.upBarColor,0);///Top
+	box(node.width,iconWidth*2.f,node.positionX * zoomVal,node.positionY * zoomVal - node.height - iconWidth*2.f,"",node.backColor,0,0,0,0.9999f,10000,node.backColor,0);///Bottom
 	//-Smooth corners
 	glUseProgram(programs.iconsProgram);
-	iconBox(iconWidth , iconWidth*2.f , node.positionX-node.width -iconWidth, node.positionY + node.height + iconWidth*2.f, 0.9999f , icons.TL , 0 , node.upBarColor , node.backColor);
-	iconBox(iconWidth , iconWidth*2.f , node.positionX+node.width +iconWidth, node.positionY + node.height + iconWidth*2.f, 0.9999f , icons.TR , 0 , node.upBarColor , node.backColor);
-	iconBox(iconWidth , iconWidth*2.f , node.positionX-node.width -iconWidth, node.positionY - node.height - iconWidth*2.f, 0.9999f , icons.BL , 0 , node.backColor , node.backColor);
-	iconBox(iconWidth , iconWidth*2.f , node.positionX+node.width +iconWidth, node.positionY - node.height - iconWidth*2.f, 0.9999f , icons.BR , 0 , node.backColor , node.backColor);
+	iconBox(iconWidth , iconWidth*2.f , node.positionX * zoomVal-node.width -iconWidth, node.positionY * zoomVal + node.height + iconWidth*2.f, 0.9999f , icons.TL , 0 , node.upBarColor , node.backColor);
+	iconBox(iconWidth , iconWidth*2.f , node.positionX * zoomVal+node.width +iconWidth, node.positionY * zoomVal + node.height + iconWidth*2.f, 0.9999f , icons.TR , 0 , node.upBarColor , node.backColor);
+	iconBox(iconWidth , iconWidth*2.f , node.positionX * zoomVal-node.width -iconWidth, node.positionY * zoomVal - node.height - iconWidth*2.f, 0.9999f , icons.BL , 0 , node.backColor , node.backColor);
+	iconBox(iconWidth , iconWidth*2.f , node.positionX * zoomVal+node.width +iconWidth, node.positionY * zoomVal - node.height - iconWidth*2.f, 0.9999f , icons.BR , 0 , node.backColor , node.backColor);
 	
 	int ioIndex = 0; //Interpret the input with output size since they both rendered seperately
 	for (size_t i = 0; i < node.outputs.size(); i++)
@@ -139,8 +139,8 @@ float maxScreenWidth,float maxScreenHeight, std::vector<Node> &nodes){
 		//If output pressed (move the connection)
 		if(glfwGetMouseButton(window,0) == GLFW_PRESS && node.outputs[i].connectionHover && !node.outputs[i].pressed){
 			node.outputs[i].pressed = true;
-			node.outputs[i].connectionPosX = node.positionX+node.width +iconWidth*2.f;
-			node.outputs[i].connectionPosY = (node.positionY + node.height) - i/(20.f/(node.width*15)) - 0.05f * node.width*10;
+			node.outputs[i].connectionPosX = node.positionX * zoomVal+node.width +iconWidth*2.f;
+			node.outputs[i].connectionPosY = (node.positionY * zoomVal + node.height) - i/(20.f/(node.width*15)) - 0.05f * node.width*10;
 			firstRelease = true;
 		}
 		//If output released (release the connection)
@@ -154,8 +154,8 @@ float maxScreenWidth,float maxScreenHeight, std::vector<Node> &nodes){
 				}
 				
 				//Render the connection on top of the default connection circle (hide)
-				node.outputs[i].connectionPosX = node.positionX+node.width +iconWidth*2.f;
-				node.outputs[i].connectionPosY = (node.positionY + node.height) - i/(20.f/(node.width*15)) - 0.05f * node.width*10;
+				node.outputs[i].connectionPosX = node.positionX * zoomVal+node.width +iconWidth*2.f;
+				node.outputs[i].connectionPosY = (node.positionY * zoomVal + node.height) - i/(20.f/(node.width*15)) - 0.05f * node.width*10;
 				
 				//Render the connection on top of the connection circle (show)
 				if(node.outputs[i].nodeConnectionIndex != 10000 && node.outputs[i].inputConnectionIndex != 10000){
@@ -173,14 +173,14 @@ float maxScreenWidth,float maxScreenHeight, std::vector<Node> &nodes){
 
         //Render the connection line
         glUseProgram(programs.uiProgram);
-		drawLine(node.positionX+node.width +iconWidth*2.f,(node.positionY + node.height) - i/(20.f/(node.width*15)) - 0.05f * node.width*10,0.99999f,node.outputs[i].connectionPosX,node.outputs[i].connectionPosY,0,nodeColor);
+		drawLine(node.positionX * zoomVal+node.width +iconWidth*2.f,(node.positionY * zoomVal + node.height) - i/(20.f/(node.width*15)) - 0.05f * node.width*10,0.99999f,node.outputs[i].connectionPosX,node.outputs[i].connectionPosY,0,nodeColor);
 
         //Render the output
 		//TODO : Use those values for rendering and tracking
         glUseProgram(programs.iconsProgram);
-		node.outputs[i].posX = node.positionX+node.width +iconWidth*2.f;
-		node.outputs[i].posY = (node.positionY + node.height) - i/(20.f/(node.width*15)) - 0.05f * node.width*10;
-		iconBox(iconWidth/1.5f , iconWidth*1.5f , node.positionX+node.width +iconWidth*2.f, (node.positionY + node.height) - i/(20.f/(node.width*15)) - 0.05f * node.width*10, 0.99999f , icons.Circle , 0 , nodeColor , nodeColor);
+		node.outputs[i].posX = node.positionX * zoomVal+node.width +iconWidth*2.f;
+		node.outputs[i].posY = (node.positionY * zoomVal + node.height) - i/(20.f/(node.width*15)) - 0.05f * node.width*10;
+		iconBox(iconWidth/1.5f , iconWidth*1.5f , node.positionX * zoomVal+node.width +iconWidth*2.f, (node.positionY * zoomVal + node.height) - i/(20.f/(node.width*15)) - 0.05f * node.width*10, 0.99999f , icons.Circle , 0 , nodeColor , nodeColor);
 		ioIndex++;
 	}
 
@@ -201,12 +201,12 @@ float maxScreenWidth,float maxScreenHeight, std::vector<Node> &nodes){
 			nodeColor = colorData.vec3NodeInputColor;
 		}
 
-		node.inputs[i].connectionHover = isMouseOnButton(window,iconWidth/1.5f , iconWidth*1.5f,node.positionX-node.width - iconWidth*2.f,(node.positionY + node.height) - (i+ioIndex+rangeBarCountInputs)/(20.f/(node.width*16)) - 0.05f * node.width*10,mouseX,mouseY,false);
+		node.inputs[i].connectionHover = isMouseOnButton(window,iconWidth/1.5f , iconWidth*1.5f,node.positionX * zoomVal-node.width - iconWidth*2.f,(node.positionY * zoomVal + node.height) - (i+ioIndex+rangeBarCountInputs)/(20.f/(node.width*16)) - 0.05f * node.width*10,mouseX,mouseY,false);
 
-		node.inputs[i].posX = node.positionX-node.width - iconWidth*2.f;
-		node.inputs[i].posY = (node.positionY + node.height) - (i+ioIndex+rangeBarCountInputs)/(20.f/(node.width*16)) - 0.05f * node.width*10;
+		node.inputs[i].posX = node.positionX * zoomVal-node.width - iconWidth*2.f;
+		node.inputs[i].posY = (node.positionY * zoomVal + node.height) - (i+ioIndex+rangeBarCountInputs)/(20.f/(node.width*16)) - 0.05f * node.width*10;
 
-		iconBox(iconWidth/1.5f , iconWidth*1.5f , node.positionX-node.width - iconWidth*2.f, (node.positionY + node.height) - (i+ioIndex+rangeBarCountInputs)/(20.f/(node.width*16)) - 0.05f * node.width*10, 0.99999f , icons.Circle , 0 , nodeColor , nodeColor);
+		iconBox(iconWidth/1.5f , iconWidth*1.5f , node.positionX * zoomVal-node.width - iconWidth*2.f, (node.positionY * zoomVal + node.height) - (i+ioIndex+rangeBarCountInputs)/(20.f/(node.width*16)) - 0.05f * node.width*10, 0.99999f , icons.Circle , 0 , nodeColor , nodeColor);
 		
         //Process the gap necessary related to the input type after rendering
 		if(node.inputs[i].type == "float"){
@@ -224,12 +224,12 @@ float maxScreenWidth,float maxScreenHeight, std::vector<Node> &nodes){
 	glUseProgram(programs.uiProgram);
 
     //Render the node title
-	renderText(programs.uiProgram,node.title,node.positionX-node.width -iconWidth,node.positionY + node.height + iconWidth*1.f,node.width/300.f);
+	renderText(programs.uiProgram,node.title,node.positionX * zoomVal-node.width -iconWidth,node.positionY * zoomVal + node.height + iconWidth*1.f,node.width/300.f);
 	
     //Render the output titles
 	for (size_t i = 0; i < node.outputs.size(); i++)
 	{
-		renderText(programs.uiProgram,node.outputs[i].text,node.positionX+node.width - (node.outputs[i].text.size()/60.f)*node.width*8.f,(node.positionY + node.height) - i/(20.f/(node.width*16)) - 0.05f * node.width*10,node.width/300.f);
+		renderText(programs.uiProgram,node.outputs[i].text,node.positionX * zoomVal+node.width - (node.outputs[i].text.size()/60.f)*node.width*8.f,(node.positionY * zoomVal + node.height) - i/(20.f/(node.width*16)) - 0.05f * node.width*10,node.width/300.f);
 	}
 		
 
@@ -257,7 +257,7 @@ float maxScreenWidth,float maxScreenHeight, std::vector<Node> &nodes){
 
 
         //Render the input title
-		renderText(programs.uiProgram,node.inputs[i].text,node.positionX-node.width -iconWidth + 0.015f,(node.positionY + node.height) - (i+ioIndex+inputElementIndex)/(20.f/(node.width*16)) - 0.05f * node.width*10,node.width/300.f);
+		renderText(programs.uiProgram,node.inputs[i].text,node.positionX * zoomVal-node.width -iconWidth + 0.015f,(node.positionY * zoomVal + node.height) - (i+ioIndex+inputElementIndex)/(20.f/(node.width*16)) - 0.05f * node.width*10,node.width/300.f);
 		inputElementIndex++;
 		
         //Render the range bars
@@ -302,14 +302,14 @@ float maxScreenWidth,float maxScreenHeight, std::vector<Node> &nodes){
 			float pointPosVal = val / (1.f/(node.width*2.f)) - node.width;
 			
 			//Range bar
-			box(node.width,iconWidth*2.f,node.positionX,(node.positionY + node.height) - (i+ioIndex+inputElementIndex)/(20.f/(node.width*16)) - 0.05f * node.width*10,"",colorData.rangeBarBack,0,0,0,0.99998f,8 / (node.width*6),node.backColor,0);///Bottom
+			box(node.width,iconWidth*2.f,node.positionX * zoomVal,(node.positionY * zoomVal + node.height) - (i+ioIndex+inputElementIndex)/(20.f/(node.width*16)) - 0.05f * node.width*10,"",colorData.rangeBarBack,0,0,0,0.99998f,8 / (node.width*6),node.backColor,0);///Bottom
 			//Pointer
-			box(iconWidth/4.f,iconWidth*2.f,node.positionX + pointPosVal,(node.positionY + node.height) - (i+ioIndex+inputElementIndex)/(20.f/(node.width*16)) - 0.05f * node.width*10,"",colorData.rangeBarFront,0,0,0,0.99999f,8 / (node.width*6),node.backColor,0);///Bottom
+			box(iconWidth/4.f,iconWidth*2.f,node.positionX * zoomVal + pointPosVal,(node.positionY * zoomVal + node.height) - (i+ioIndex+inputElementIndex)/(20.f/(node.width*16)) - 0.05f * node.width*10,"",colorData.rangeBarFront,0,0,0,0.99999f,8 / (node.width*6),node.backColor,0);///Bottom
 
 			//TODO : Calculate the screen gap
             //Check if range bar pointer pressed
 			if(!anyPointerPressed){
-				isRangeBarPointerHover = isMouseOnButton(window , iconWidth , iconWidth*2.f ,node.positionX + pointPosVal,(node.positionY + node.height) - (i+ioIndex+inputElementIndex)/(20.f/(node.width*16)) - 0.05f * node.width*10,mouseX,mouseY,false);
+				isRangeBarPointerHover = isMouseOnButton(window , iconWidth , iconWidth*2.f ,node.positionX * zoomVal + pointPosVal,(node.positionY * zoomVal + node.height) - (i+ioIndex+inputElementIndex)/(20.f/(node.width*16)) - 0.05f * node.width*10,mouseX,mouseY,false);
 			}
 			if(glfwGetMouseButton(window,0) == GLFW_PRESS && isRangeBarPointerHover){
 				node.inputs[i].rangeBarsPointerPressed[k] = true;
