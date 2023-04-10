a57fd2465ef24bb6b3d46331e1157f5a37cd8efe
mert-tetik <merttetikx@icloud.com>
2023-04-11 01:29:17 +0300

    PBR node - Out options

--- LigidPainter/Main/src/Core/Application.cpp
+++ LigidPainter/Main/src/Core/Application.cpp
@@ -36,7 +36,6 @@
 //Left CTRL + TAB + T = Switch to painting panel
 //Left CTRL + TAB + R = Switch to export panel
 
-//TODO Fix node lagging
 //TODO Fix mirror origin pos
 //TODO Fix mirror paint over
 //TODO Change texture extension
