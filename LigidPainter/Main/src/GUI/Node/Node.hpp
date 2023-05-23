12200db96d62f21e43311452a25785c8bab51ac9
mert-tetik <merttetikx@icloud.com>
2023-05-23 23:02:03 +0300

    Node.hpp

---
+++ LigidPainter/Main/src/GUI/Node/Node.hpp
@@ -0,0 +1,45 @@
+#ifndef LGDUINODE_HPP
+#define LGDUINODE_HPP
+
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "assimp/Importer.hpp"
+#include "assimp/scene.h"
+#include "assimp/postprocess.h"
+
+#include "Shader.hpp"
+#include "Renderer.h"
+#include "Util.hpp"
+#include "Timer.hpp"
+#include "Texture.hpp"
+#include "Mouse.hpp"
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <cstdlib>
+
+class NodeIO;
+
+class Node
+{
+private:
+    /* data */
+public:
+    std::vector<NodeIO> inputs;
+    std::vector<NodeIO> outputs;
+
+    node(/* args */){
+
+    }
+};
+
+#endif
