1415a4dc5b9d8eec41c45da52f21be660c37b2b3
mert-tetik <merttetikx@icloud.com>
2023-05-25 03:09:52 +0300

    Material.hpp

---
+++ LigidPainter/Main/src/3D/Material.hpp
@@ -0,0 +1,50 @@
+/*
+---------------------------------------------------------------------------
+LigidPainter - 3D Model texturing software / Texture generator   
+---------------------------------------------------------------------------
+
+Copyright (c) 2022-2023, LigidTools 
+
+All rights reserved.
+
+Official GitHub Link : https://github.com/mert-tetik/LigidPainter
+Official Web Page : https://ligidtools.com/ligidpainter
+
+---------------------------------------------------------------------------
+*/
+
+#ifndef LGD3DMATERIAL_HPP
+#define LGD3DMATERIAL_HPP
+
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "Shader.hpp"
+#include "Box.hpp"
+#include "Renderer.h"
+#include "Util.hpp"
+#include "Timer.hpp"
+#include "Texture.hpp"
+
+#include <glm/gtc/type_ptr.hpp>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <cstdlib>
+
+class Material
+{
+private:
+    /* data */
+public:
+    Material(/* args */){}
+};
+
+#endif
