1450775dd5718997ed18cc48bd08c0387e444358
mert-tetik <merttetikx@icloud.com>
2023-12-04 20:27:56 +0300

    Material displayer dialog fixed

--- LigidPainter/Main/src/GUI/Dialogs/MaterialDisplayer.cpp
+++ LigidPainter/Main/src/GUI/Dialogs/MaterialDisplayer.cpp
@@ -79,11 +79,14 @@ void MaterialDisplayerDialog::render(Timer timer){
      
     //Close the dialog
     if(getContext()->window.isKeyPressed(LIGIDGL_KEY_ESCAPE) == LIGIDGL_PRESS || (!panel.hover && *Mouse::LClick()) || (panel.sections[0].elements[0].button.hover && *Mouse::LDoubleClick())){
-        if(!dialogControl.firstFrameActivated)
+        if(!dialogControl.firstFrameActivated){
             this->dialogControl.unActivate();
+            this->material.updateMaterialDisplayingTexture(512, this->dialogControl.firstFrameActivated, Camera(), 0, false);
+        }
     }
-
-    this->material.updateMaterialDisplayingTexture(512, this->dialogControl.firstFrameActivated, this->displayingCam, 0, true);
+    
+    if(this->dialogControl.active)
+        this->material.updateMaterialDisplayingTexture(512, this->dialogControl.firstFrameActivated, this->displayingCam, 0, true);
     
     dialogControl.updateEnd(timer,0.15f);
 }
